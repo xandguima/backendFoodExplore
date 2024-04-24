ff48e68b98de43193aec05510f8c95b8a73c7fe9
xandguima <xan.96@hotmail.com>
2024-04-23 21:28:04 -0300

    Created IngredientsController

---
+++ src/controllers/IngredientsController.js
@@ -0,0 +1,41 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+
+class IngredientsController {
+  async add(request, response) {
+    const user_id = request.user.id
+    const { dish_id } = request.params
+    const {ingredients} = request.body;
+
+    const dish = await knex("dish").where({ id:dish_id }).first()
+
+    if (!dish) {
+      throw new AppError("Prato não cadastrado");
+    }
+
+    const listIngredients = ingredients.map(ingredient => {
+      return {
+        dish_id,
+        user_id,
+        name: ingredient
+      }
+    });
+    try {
+      await knex("ingredients").insert(listIngredients);
+
+    } catch (error) {
+      throw new AppError(error);
+    }
+
+    return response.status(201).json();
+  }
+
+  async delete(request, response) {
+    const { ingredient_id } = request.params
+
+    await knex("ingredients").where({ id:ingredient_id }).delete();
+    
+    return response.status(200).json();
+  }
+}
+module.exports = IngredientsController

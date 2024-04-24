b82abf39c8ec44330da4c31d1ddefbfc3f71e427
xandguima <xan.96@hotmail.com>
2024-04-23 21:29:58 -0300

    add ingredients routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,11 @@ const routes=Router();
 const userRoutes=require("./users.routes");
 const dishRoutes=require("./dish.routes");
 const sessionRoutes=require("./sessions.routes");
+const ingredientRoutes = require("./ingredients.routes");
 
 routes.use("/user",userRoutes)
 routes.use("/dish",dishRoutes)
 routes.use("/session",sessionRoutes)
+routes.use("/ingredient",ingredientRoutes)
 
 module.exports=routes

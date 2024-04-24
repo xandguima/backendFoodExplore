6768a3f1c38a1d5d5c3886dcc218212b61f2fa2b
xandguima <xan.96@hotmail.com>
2024-04-23 21:30:09 -0300

    config ingredientsRoutes

---
+++ src/routes/ingredients.routes.js
@@ -0,0 +1,17 @@
+const {Router}=require("express");
+const ensureAuthenticated=require("../middlewares/ensureAuthenticated")
+
+const IngredientsController = require("../controllers/IngredientsController")
+
+const ingredientsController = new IngredientsController()
+
+const ingredientRoutes=Router();
+
+ingredientRoutes.use(ensureAuthenticated);
+
+
+ingredientRoutes.post("/:dish_id",ingredientsController.add);
+ingredientRoutes.delete("/:ingredient_id",ingredientsController.delete)
+
+
+module.exports = ingredientRoutes

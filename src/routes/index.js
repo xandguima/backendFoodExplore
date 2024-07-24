0fa509ce1a97a7ec50fb3b3ce370df35ea96fe80
Alexandre <devxandeapp@gmail.com>
2024-07-24 12:50:01 -0300

    add likeRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,13 @@ const userRoutes=require("./users.routes");
 const dishRoutes=require("./dish.routes");
 const sessionRoutes=require("./sessions.routes");
 const ingredientRoutes = require("./ingredients.routes");
+const likeRoutes = require("./like.routes");
 
 routes.use("/user",userRoutes);
 routes.use("/dish",dishRoutes);
 routes.use("/session",sessionRoutes);
 routes.use("/ingredient",ingredientRoutes);
+routes.use("/like",likeRoutes);
 
-module.exports=routes
+
+module.exports = routes

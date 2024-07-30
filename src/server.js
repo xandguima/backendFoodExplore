f4e4d90eb38d1e507009bc2424701826e8594ef0
Alexandre <devxandeapp@gmail.com>
2024-07-30 16:31:36 -0300

    adjust origin for cors

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ app.use(cookieParser());
 
 app.use(cors(
   {
-    origin: ['http://localhost:5173',"http://127.0.0.1:5173/"],
+    origin: ['foodexplorerocketseat.netlify.app'],
     credentials: true
   }
 ))

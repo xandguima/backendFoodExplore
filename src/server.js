c8f407f9b6f5178e074e0f9898470c5e7711ca52
Alexandre <devxandeapp@gmail.com>
2024-07-30 16:57:45 -0300

    update cors

--- src/server.js
+++ src/server.js
@@ -1,25 +1,29 @@
 require("express-async-errors");
-require("dotenv/config")
+require("dotenv/config");
 
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const express =require("express");
-const AppError=require("./utils/AppError");
-const uploadConfig=require("./config/upload");
+const express = require("express");
+const AppError = require("./utils/AppError");
+const uploadConfig = require("./config/upload");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors(
-  {
-    origin: ['https://foodexplorerocketseat.netlify.app/','http://localhost:5173',"http://127.0.0.1:5173/"],
-    credentials: true
-  }
-))
+app.use(cors({
+  origin: ['https://foodexplorerocketseat.netlify.app', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 
+app.options('*', cors({
+  origin: ['https://foodexplorerocketseat.netlify.app', 'http://localhost:5173', 'http://127.0.0.1:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
 
-const routes=require("./routes");
+const routes = require("./routes");
 app.use(routes);
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
@@ -39,9 +43,8 @@ app.use((error, request, response, next) => {
   });
 });
 
-
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

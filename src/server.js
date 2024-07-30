3a5b49ee7fe2083b87dab1dc9c9090bc62b6490d
Alexandre <devxandeapp@gmail.com>
2024-07-30 17:07:18 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,25 @@
-require("express-async-errors");
-require("dotenv/config");
-
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const AppError = require("./utils/AppError");
 const uploadConfig = require("./config/upload");
 
-const app = express();
-app.use(express.json());
-app.use(cookieParser());
+require("express-async-errors");
+require("dotenv/config");
 
+const app = express();
+
+// Middleware global para CORS
 app.use(cors({
   origin: ['https://foodexplorerocketseat.netlify.app', 'http://localhost:5173', 'http://127.0.0.1:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true
 }));
 
+app.use(express.json());
+app.use(cookieParser());
+
+// Middleware de CORS para opções
 app.options('*', cors({
   origin: ['https://foodexplorerocketseat.netlify.app', 'http://localhost:5173', 'http://127.0.0.1:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -31,15 +34,15 @@ app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 app.use((error, request, response, next) => {
   console.log(error);
   if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-          status: "error",
-          message: error.message
-      });
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message
+    });
   }
 
   return response.status(500).json({
-      status: "error",
-      message: "internal server error"
+    status: "error",
+    message: "internal server error"
   });
 });
 

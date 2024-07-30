d3b4c8134d92d47310f75c09f7bc4ca3128c8234
Alexandre <devxandeapp@gmail.com>
2024-07-30 17:31:06 -0300

    update cors

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware global para CORS
 app.use(cors({
-  origin: 'https://foodexplorerocketseat.netlify.app',
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true
 }));
 
